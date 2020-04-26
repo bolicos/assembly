9a0e5860eb97389932304390147c9d90342e4353
analuciabolico <analuciabolico.dev@gmail.com>
2020-04-26 19:13:42 -0300

    #4 - Remove Security Config

--- src/main/java/com/analuciabolico/assembly/v1/config/WebConfig.java
+++ src/main/java/com/analuciabolico/assembly/v1/config/WebConfig.java
@@ -1,9 +1,6 @@
 package com.analuciabolico.assembly.v1.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,19 +12,5 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-    }
-
-    @Bean
-    public WebSecurityConfigurerAdapter corsConfigurer() {
-        return new WebSecurityConfigurerAdapter() {
-            @Override
-            protected void configure(HttpSecurity http) throws Exception {
-                http.httpBasic().and().authorizeRequests()
-                        .antMatchers("/h2/**", "/api/v1/health").permitAll()
-                        .anyRequest().authenticated();
-                http.csrf().ignoringAntMatchers("/h2/**");
-                http.headers().frameOptions().sameOrigin();
-            }
-        };
     }
 }
