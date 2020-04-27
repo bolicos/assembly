d1253517d7fad2768bb67bada4e9d81b46097a73
analuciabolico <lucia.bolico@4all.com>
2020-04-27 00:06:52 -0300

    #23 - Document the schedule routes

--- src/main/java/com/analuciabolico/assembly/v1/api/schedule/ScheduleController.java
+++ src/main/java/com/analuciabolico/assembly/v1/api/schedule/ScheduleController.java
@@ -7,6 +7,8 @@ import com.analuciabolico.assembly.v1.schedule.dto.ScheduleSessionDto;
 import com.analuciabolico.assembly.v1.schedule.enums.ScheduleResultEnum;
 import com.analuciabolico.assembly.v1.schedule.model.Schedule;
 import com.analuciabolico.assembly.v1.schedule.service.interfaces.IScheduleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "Associated")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/schedule")
@@ -23,27 +26,32 @@ public class ScheduleController {
 
     private final IScheduleService scheduleService;
 
+    @ApiOperation(value = "Find all schedules")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Schedule>> getAll(@RequestParam(required = false, defaultValue = "ASC") String sort) {
         List<Schedule> list = scheduleService.findAll(Sort.by(Sort.Direction.fromString(sort), "title"));
         return list.isEmpty() ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(list, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Find a schedule by ID")
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Schedule> findById(@PathVariable Long id) {
         return new ResponseEntity<>(scheduleService.findById(id), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Saves the data of a schedule")
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ResourceCreated> save(@RequestBody ScheduleDto schedule) {
         return new ResponseEntity<>(scheduleService.save(schedule), HttpStatus.CREATED);
     }
 
+    @ApiOperation(value = "Creates a voting session on a schedule")
     @PatchMapping(value = "/{id}/session", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Schedule> session(@PathVariable Long id, @RequestBody(required = false) ScheduleSessionDto schedule) {
         return new ResponseEntity<>(scheduleService.session(schedule, id), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Calculates the result of the vote on a schedule")
     @PatchMapping(value = "/{id}/result", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ScheduleResultEnum> result(@PathVariable Long id, @RequestBody ScheduleResultDto status) {
         return new ResponseEntity<>(scheduleService.result(status, id), HttpStatus.OK);
