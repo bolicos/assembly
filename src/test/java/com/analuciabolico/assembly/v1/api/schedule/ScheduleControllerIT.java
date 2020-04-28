b5533db8998fa093e9ae73302ad1f901056749e0
analuciabolico <analuciabolico.dev@gmail.com>
2020-04-28 00:08:41 -0300

    #33 - creates test that fetches all schedules

--- src/test/java/com/analuciabolico/assembly/v1/api/schedule/ScheduleControllerIT.java
+++ src/test/java/com/analuciabolico/assembly/v1/api/schedule/ScheduleControllerIT.java
@@ -1,6 +1,5 @@
 package com.analuciabolico.assembly.v1.api.schedule;
 
-import com.analuciabolico.assembly.v1.associated.model.Associated;
 import com.analuciabolico.assembly.v1.core.BaseControllerIT;
 import com.analuciabolico.assembly.v1.schedule.model.Schedule;
 import com.analuciabolico.assembly.v1.schedule.repository.ScheduleRepository;
@@ -11,14 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 
-import static com.analuciabolico.assembly.v1.core.Constants.DESCRIPTION;
-import static com.analuciabolico.assembly.v1.core.Constants.INVALID_ID;
-import static com.analuciabolico.assembly.v1.core.Constants.NONEXISTENT_ID;
-import static com.analuciabolico.assembly.v1.core.Constants.ONE_LONG;
-import static com.analuciabolico.assembly.v1.core.Constants.TITLE;
-import static com.analuciabolico.assembly.v1.core.Constants.VALID_CPF;
+import static com.analuciabolico.assembly.v1.core.Constants.*;
+import static com.analuciabolico.assembly.v1.core.SqlDocumentProvider.INSERT_LIST_SCHEDULES;
 import static com.analuciabolico.assembly.v1.core.SqlDocumentProvider.INSERT_SCHEDULE;
 import static com.analuciabolico.assembly.v1.core.SqlDocumentProvider.REMOVE_SCHEDULE;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -41,7 +37,7 @@ class ScheduleControllerIT extends BaseControllerIT {
 
     @DisplayName("Find Schedule By Id")
     void findScheduleById() throws Exception {
-        mockMvc.perform(get("/api/v1/schedule/" + ONE_LONG))
+        mockMvc.perform(get("/api/v1/schedules/" + ONE_LONG))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", notNullValue()))
                 .andExpect(jsonPath("$.title", containsString(TITLE)))
@@ -51,26 +47,42 @@ class ScheduleControllerIT extends BaseControllerIT {
     @Test
     @DisplayName("Find Schedule Not Exist")
     void findScheduleNotExist() throws Exception {
-        mockMvc.perform(get("/api/v1/schedule/" + NONEXISTENT_ID))
+        mockMvc.perform(get("/api/v1/schedules/" + NONEXISTENT_ID))
                 .andExpect(status().isNotFound());
     }
 
     @Test
     @DisplayName("Find Schedule Invalid Parameter")
     void findScheduleInvalidParameter() throws Exception {
-        mockMvc.perform(get("/api/v1/schedule/" + INVALID_ID))
+        mockMvc.perform(get("/api/v1/schedules/" + INVALID_ID))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
     @DisplayName("Save Schedule")
     void saveSchedule() throws Exception {
-        mockMvc.perform(post("/api/v1/schedule")
+        mockMvc.perform(post("/api/v1/schedules")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content(this.mapper.writeValueAsString(oneScheduleDto())))
                 .andExpect(status().isCreated());
 
         Schedule schedule = scheduleRepository.findByTitle(TITLE);
         assertEquals(oneScheduleDto().getTitle(), schedule.getTitle());
+    }
+
+    @Test
+    @SqlGroup({
+            @Sql(scripts = {REMOVE_SCHEDULE}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+            @Sql(scripts = {INSERT_LIST_SCHEDULES}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+            @Sql(scripts = {REMOVE_SCHEDULE}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    })
+    @DisplayName("Find all Schedules")
+    void findAllSchedules() throws Exception {
+        mockMvc.perform(get("/api/v1/schedules/"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[*].id", notNullValue()))
+                .andExpect(jsonPath("$.[*].id", containsInAnyOrder(LIST_INTEGER)))
+                .andExpect(jsonPath("$.[*].title", containsInAnyOrder(LIST_TITLES)))
+                .andExpect(jsonPath("$.[*].description", containsInAnyOrder(LIST_DESCRIPTIONS)));
     }
 }
