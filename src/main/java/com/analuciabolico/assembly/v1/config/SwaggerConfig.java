b4223ebc9572a88357c284705288b88e2c5b1345
analuciabolico <analuciabolico.dev@gmail.com>
2020-04-27 21:06:42 -0300

    adjustment

--- src/main/java/com/analuciabolico/assembly/v1/config/SwaggerConfig.java
+++ src/main/java/com/analuciabolico/assembly/v1/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig extends WebConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.analuciabolico.assembly.v1.api"))
                 .paths(PathSelectors.any())
                 .build();
     }
