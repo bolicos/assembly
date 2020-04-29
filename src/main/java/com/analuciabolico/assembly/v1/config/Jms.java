49a2fb327856482e32d4d8162089997c7014242c
analuciabolico <lucia.bolico@4all.com>
2020-04-29 01:29:18 -0300

    #30 - add configs messagings yml and build gradle

--- src/main/java/com/analuciabolico/assembly/v1/config/Jms.java
+++ src/main/java/com/analuciabolico/assembly/v1/config/Jms.java
@@ -3,23 +3,35 @@ package com.analuciabolico.assembly.v1.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
+import javax.jms.ConnectionFactory;
+import javax.jms.Queue;
+
+@EnableJms
 @Configuration
 public class Jms {
 
     public static final String ASSEMBLY = "assembly";
 
     @Bean
-    public JmsListenerContainerFactory<?> queueListenerFactory() {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        factory.setMessageConverter(messageConverter());
+    public Queue queue() {
+        return new ActiveMQQueue("queue");
+    }
+
+    @Bean
+    public JmsListenerContainerFactory<?> myJmsContainerFactory(@Qualifier("jmsConnectionFactory") ConnectionFactory connectionFactory) {
+        SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
         return factory;
     }
 
